dc319276af7828bd065d35d1132a03161e96380e
Arto Bendiken <arto@bendiken.net>
2011-07-13 11:57:34 +0200

    Implemented the string_size() and string_length() functions.

--- src/string.c
+++ src/string.c
@@ -4,6 +4,32 @@
 #include <string.h>
 #include <strings.h>
 
+size_t
+string_size(const string_t* const string) {
+  if (unlikely(string == NULL))
+    return -EINVAL;
+
+  if (unlikely(string->size == 0))
+    return 0; // the string is empty
+
+  if (likely(string->size != STRING_SIZE_UNKNOWN))
+    return string->size;
+
+  return strlen(string->data);
+}
+
+size_t
+string_length(const string_t* const string) {
+  if (unlikely(string == NULL))
+    return -EINVAL;
+
+#ifndef _CPRIME_HAVE_UTF8
+  return string_size(string); // ASCII only
+#else
+#error UTF-8 support has not yet been implemented.
+#endif /* !_CPRIME_HAVE_UTF8 */
+}
+
 int
 string_equal(const string_t* const string1, const string_t* const string2) {
   if (unlikely(string1 == NULL || string2 == NULL))
@@ -11,6 +37,7 @@ string_equal(const string_t* const string1, const string_t* const string2) {
 
   if (unlikely(string1 == string2))
     return TRUE;
+
   if (likely(string1->size != string2->size))
     return FALSE;
 
--- src/string.h
+++ src/string.h
@@ -8,11 +8,19 @@ extern "C" {
 #endif
 
 #include <stddef.h> /* for size_t */
+#include <stdint.h> /* for SIZE_MAX */
+
+#define STRING_SIZE_UNKNOWN (SIZE_MAX)
+#define STRING_SIZE_MAX     (SIZE_MAX-1)
+#define STRING_SIZE_MIN     0
 
 typedef struct {
   char* data;
   size_t size;
 } string_t;
+
+extern size_t string_size(const string_t* const string);
+extern size_t string_length(const string_t* const string);
 
 extern int string_equal(const string_t* const string1, const string_t* const string2);
 extern int string_compare(const string_t* const string1, const string_t* const string2);
