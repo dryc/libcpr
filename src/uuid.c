505e283549719460318d0f102a143c05679c8434
Arto Bendiken <arto@bendiken.net>
2011-07-15 00:50:15 +0200

    Implemented the uuid_alloc(), uuid_init(), and uuid_is_zero() functions.

--- src/uuid.c
+++ src/uuid.c
@@ -1,3 +1,28 @@
 /* This is free and unencumbered software released into the public domain. */
 
 #include "build.h"
+#include <stdlib.h> /* for calloc(), free(), malloc() */
+
+const uuid_t uuid_zero = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+uuid_t*
+uuid_alloc() {
+  uuid_t* uuid = malloc(sizeof(uuid_t));
+  uuid_init(uuid);
+  return uuid;
+}
+
+int
+uuid_init(uuid_t* uuid) {
+  validate_with_errno_return(uuid != NULL);
+  bzero(uuid, sizeof(uuid_t));
+  return 0;
+}
+
+int
+uuid_is_zero(const uuid_t* const uuid) {
+  validate_with_errno_return(uuid != NULL);
+  return unlikely(bcmp(uuid, &uuid_zero, sizeof(uuid_t)) == 0) ? TRUE : FALSE;
+}
--- src/uuid.h
+++ src/uuid.h
@@ -7,13 +7,23 @@
 extern "C" {
 #endif
 
-#include <stdint.h> /* for uint8_t */
+#ifdef HAVE_UNISTD_H
 #include <unistd.h> /* for uuid_t on Darwin */
+#endif /* HAVE_UNISTD_H */
 
 #ifndef _UUID_T
 #define _UUID_T
+#include <stdint.h> /* for uint8_t */
 typedef uint8_t uuid_t[16];
 #endif /* _UUID_T */
+
+extern const uuid_t uuid_zero;
+
+extern uuid_t* uuid_alloc();
+
+extern int uuid_init(uuid_t* uuid);
+
+extern int uuid_is_zero(const uuid_t* const uuid);
 
 #ifdef __cplusplus
 }
