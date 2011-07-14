d57f30b23c895268bc9dd1bdd0e605d587319899
Arto Bendiken <arto@bendiken.net>
2011-07-14 08:28:48 +0200

    Defined a variadic string_init() constructor macro.

--- src/string.h
+++ src/string.h
@@ -26,13 +26,14 @@ extern string_t* string_alloc_with_data(const char* const data);
 
 extern string_t* string_clone(const string_t* const string);
 
-#define string_init0(string) string_init_empty(string)
-#define string_init1(string, arg1) \
-  __dispatch_begin(arg1) \
-    __dispatch(string_init_with_data, string, char*,  arg1, NULL) \
-    __dispatch(string_init_with_size, string, size_t, arg1, 0) \
-  __dispatch_end
-#define string_init string_init1
+#define string_init1(string) string_init_empty(string)
+#define string_init2(string, arg1) \
+  DISPATCH_BEGIN(arg1) \
+    DISPATCH(string_init_with_data, string, char*,  arg1, NULL) \
+    DISPATCH(string_init_with_size, string, size_t, arg1, 0) \
+  DISPATCH_END
+#define string_init(...) \
+  CONCAT(string_init, ARITY(__VA_ARGS__))(__VA_ARGS__)
 
 extern int string_init_empty(string_t* string);
 extern int string_init_with(string_t* string, const char* const data, const size_t size);
