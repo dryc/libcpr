ecce2cf81e1bcc9602bad60f5b1e9704c360de0a
Arto Bendiken <arto@bendiken.net>
2011-07-17 17:53:14 +0200

    Defined the cprime_module_names and cprime_module_count constants that can be used to determine with which modules libcprime was compiled.

--- src/cprime.c
+++ src/cprime.c
@@ -3,3 +3,42 @@
 #include "build.h"
 
 const char* const cprime_version_string = PACKAGE_VERSION;
+
+const char* const cprime_module_names[] = {
+  "atomic",
+  "bool",
+  "byte",
+  "char",
+  "closure",
+  "decimal",
+  "dir",
+  "file",
+  "integer",
+  "list",
+  "map",
+#ifndef DISABLE_THREADS
+  "mutex",
+  "once",
+#endif
+  "pair",
+  "posix",
+  "process",
+  "queue",
+#ifndef DISABLE_THREADS
+  "rwlock",
+#endif
+  "string",
+  "symbol",
+#ifndef DISABLE_THREADS
+  "thread",
+#endif
+  "uri",
+#ifndef DISABLE_UNICODE
+  "utf8",
+#endif
+  "uuid",
+  "vector",
+};
+
+const unsigned int cprime_module_count =
+  sizeof(cprime_module_names) / sizeof(cprime_module_names[0]);
