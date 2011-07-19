45f31590d2aa3c312bc91616702fc9d36702cdf8
Arto Bendiken <arto@bendiken.net>
2011-07-19 21:10:07 +0200

    Implemented the vector_alloc(), vector_free(), vector_init*(), and vector_dispose() functions.

--- src/vector.c
+++ src/vector.c
@@ -1,3 +1,56 @@
 /* This is free and unencumbered software released into the public domain. */
 
 #include "build.h"
+
+vector_t*
+vector_alloc() {
+  vector_t* vector = malloc(sizeof(vector_t));
+  vector_init_empty(vector);
+  return vector;
+}
+
+void
+vector_free(vector_t* vector) {
+  if (likely(vector != NULL)) {
+    vector_dispose(vector);
+    free(vector);
+  }
+}
+
+int
+vector_init_empty(vector_t* vector) {
+  validate_with_errno_return(vector != NULL);
+
+  bzero(vector, sizeof(vector_t));
+
+  return 0;
+}
+
+int
+vector_init_with(vector_t* vector, const size_t element_size, const size_t capacity) {
+  int result = vector_init_empty(vector);
+
+  if (likely(succeeded(result) && element_size > 0 && capacity > 0)) {
+    vector->data = calloc(capacity, element_size);
+  }
+  vector->element_size = element_size;
+  vector->capacity     = capacity;
+  vector->count        = 0;
+
+  return result;
+}
+
+int
+vector_dispose(vector_t* vector) {
+  validate_with_errno_return(vector != NULL);
+
+  if (likely(vector->data != NULL)) {
+    free(vector->data);
+    vector->data = NULL;
+  }
+  vector->element_size = 0;
+  vector->capacity     = 0;
+  vector->count        = 0;
+
+  return 0;
+}
--- src/vector.h
+++ src/vector.h
@@ -9,10 +9,38 @@ extern "C" {
 
 #include <stddef.h> /* for size_t */
 
-typedef struct {
-  void* data; // TODO
+#define VECTOR_LENGTH_MAX (SIZE_MAX-1)
+#define VECTOR_LENGTH_MIN 0
+
+typedef struct vector_t {
+  size_t element_size;
+  size_t capacity;
   size_t count;
+  byte_t* data;
 } vector_t;
+
+/**
+ * Allocates heap memory for a new vector.
+ */
+extern vector_t* vector_alloc();
+
+/**
+ * Releases the heap memory used by a vector.
+ */
+extern void vector_free(vector_t* vector);
+
+/**
+ * Initializes a vector.
+ */
+extern int vector_init_empty(vector_t* vector);
+extern int vector_init_with(vector_t* vector,
+  const size_t element_size,
+  const size_t capacity);
+
+/**
+ * Disposes of a vector.
+ */
+extern int vector_dispose(vector_t* vector);
 
 #ifdef __cplusplus
 }
