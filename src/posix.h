b931a24455b28b024bd0dbc725b320da6bb762b4
Arto Bendiken <arto@bendiken.net>
2011-08-26 15:00:32 +0200

    Added the missing posix_mq_timedsend() and posix_mq_timedreceive() prototypes to <cprime/posix.h>.

--- src/posix.h
+++ src/posix.h
@@ -21,16 +21,31 @@ extern int posix_poll(struct pollfd fds[], const nfds_t nfds, const int timeout)
 #ifdef HAVE_PTHREAD_H
 #include <pthread.h> /* for pthread_t, pthread_attr_t */
 extern int posix_pthread_create(pthread_t* restrict thread,
-                                const pthread_attr_t* restrict attr,
-                                void* (*start_routine)(void*), void* restrict arg);
+  const pthread_attr_t* restrict attr,
+  void* (*start_routine)(void*), void* restrict arg);
 #endif /* HAVE_PTHREAD_H */
 
 #ifdef HAVE_MQUEUE_H
+#include <time.h>   /* for struct timespec */
 #include <mqueue.h> /* for mqd_t */
-extern mqd_t posix_mq_send(mqd_t mqdes, const char* msg_ptr,
-                           size_t msg_len, unsigned int msg_prio);
-extern ssize_t posix_mq_receive(mqd_t mqdes, char* msg_ptr,
-                                size_t msg_len, unsigned int* msg_prio);
+extern mqd_t posix_mq_send(mqd_t mqdes,
+  const char* msg_ptr,
+  size_t msg_len,
+  unsigned int msg_prio);
+extern mqd_t posix_mq_timedsend(mqd_t mqdes,
+  const char* msg_ptr,
+  size_t msg_len,
+  unsigned int msg_prio,
+  const struct timespec* abs_timeout);
+extern ssize_t posix_mq_receive(mqd_t mqdes,
+  char* msg_ptr,
+  size_t msg_len,
+  unsigned int* msg_prio);
+extern ssize_t posix_mq_timedreceive(mqd_t mqdes,
+  char* msg_ptr,
+  size_t msg_len,
+  unsigned int* msg_prio,
+  const struct timespec* abs_timeout);
 #endif /* HAVE_MQUEUE_H */
 
 #ifdef __cplusplus
