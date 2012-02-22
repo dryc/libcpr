956ba5421d34c21b8e381d8bab8f97a7b820b6a3
Arto Bendiken <arto@bendiken.net>
2012-02-22 17:55:53 +0100

    Implemented a simple `make check`-compatible unit test harness.

---
+++ test/check_map.c
@@ -0,0 +1,10 @@
+#include <assert.h> /* for assert() */
+#include <stdio.h>  /* for printf() */
+#include <stdlib.h> /* for EXIT_SUCCESS */
+
+#include "cprime.h"
+
+int
+main(void) {
+  return EXIT_SUCCESS; // TODO
+}
