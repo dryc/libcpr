f74ebfe584235fccd8c660131ef16be78b475c29
Arto Bendiken <arto@bendiken.net>
2012-06-16 13:38:25 +0200

    Implemented the int32_sort() and int64_sort() functions.

--- src/sort.c
+++ src/sort.c
@@ -4,11 +4,42 @@
 #include <stdlib.h> /* for qsort() */
 
 static int
+int32_compare(const int32_t* const restrict a,
+              const int32_t* const restrict b) {
+  if (*a < *b) return COMPARE_LT;
+  if (*a > *b) return COMPARE_GT;
+  return COMPARE_EQ;
+}
+
+static int
 uint32_compare(const uint32_t* const restrict a,
                const uint32_t* const restrict b) {
   if (*a < *b) return COMPARE_LT;
   if (*a > *b) return COMPARE_GT;
   return COMPARE_EQ;
+}
+
+static int
+int64_compare(const int64_t* const restrict a,
+              const int64_t* const restrict b) {
+  if (*a < *b) return COMPARE_LT;
+  if (*a > *b) return COMPARE_GT;
+  return COMPARE_EQ;
+}
+
+static int
+uint64_compare(const uint64_t* const restrict a,
+               const uint64_t* const restrict b) {
+  if (*a < *b) return COMPARE_LT;
+  if (*a > *b) return COMPARE_GT;
+  return COMPARE_EQ;
+}
+
+public void
+int32_sort(int32_t* const restrict data,
+           const size_t count) {
+  if (count <= 1) return;
+  qsort(data, count, sizeof(int32_t), (compare_func_t)int32_compare);
 }
 
 public void
@@ -18,12 +49,11 @@ uint32_sort(uint32_t* const restrict data,
   qsort(data, count, sizeof(uint32_t), (compare_func_t)uint32_compare);
 }
 
-static int
-uint64_compare(const uint64_t* const restrict a,
-               const uint64_t* const restrict b) {
-  if (*a < *b) return COMPARE_LT;
-  if (*a > *b) return COMPARE_GT;
-  return COMPARE_EQ;
+public void
+int64_sort(int64_t* const restrict data,
+           const size_t count) {
+  if (count <= 1) return;
+  qsort(data, count, sizeof(int64_t), (compare_func_t)int64_compare);
 }
 
 public void
--- src/sort.h
+++ src/sort.h
@@ -8,12 +8,22 @@ extern "C" {
 #endif
 
 #include <stddef.h> /* for size_t */
-#include <stdint.h> /* for uint32_t, uint64_t */
+#include <stdint.h> /* for int*_t, uint*_t */
+
+/**
+ * Sorts a vector of signed 32-bit integers.
+ */
+void int32_sort(int32_t* data, size_t count);
 
 /**
  * Sorts a vector of unsigned 32-bit integers.
  */
 void uint32_sort(uint32_t* data, size_t count);
+
+/**
+ * Sorts a vector of signed 64-bit integers.
+ */
+void int64_sort(int64_t* data, size_t count);
 
 /**
  * Sorts a vector of unsigned 64-bit integers.
