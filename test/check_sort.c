912b7af8e12dbf4ff081ea2a931135ea829da7cb
Arto Bendiken <arto@bendiken.net>
2012-06-18 16:58:46 +0200

    Added basic unit tests for int32_sort() and int64_sort().

--- test/check_sort.c
+++ test/check_sort.c
@@ -7,6 +7,30 @@
 #define COUNT 1000
 
 static void
+check_int32_sort(void) {
+  int32_t* const data = calloc(COUNT, sizeof(int32_t));
+  assert(is_nonnull(data));
+
+  for (unsigned int i = 0; i < COUNT; i++) {
+    data[i] = (int32_t)rand() * (rand() % 2 ? 1 : -1);
+  }
+
+  int32_sort(data, COUNT);
+
+  {
+    int32_t previous = INT32_MIN;
+    int32_t current;
+    for (unsigned int i = 0; i < COUNT; i++) {
+      current = data[i];
+      assert(current >= previous);
+      previous = current;
+    }
+  }
+
+  free(data);
+}
+
+static void
 check_uint32_sort(void) {
   uint32_t* const data = calloc(COUNT, sizeof(uint32_t));
   assert(is_nonnull(data));
@@ -18,7 +42,32 @@ check_uint32_sort(void) {
   uint32_sort(data, COUNT);
 
   {
-    uint32_t previous = 0, current = 0;
+    uint32_t previous = 0;
+    uint32_t current;
+    for (unsigned int i = 0; i < COUNT; i++) {
+      current = data[i];
+      assert(current >= previous);
+      previous = current;
+    }
+  }
+
+  free(data);
+}
+
+static void
+check_int64_sort(void) {
+  int64_t* const data = calloc(COUNT, sizeof(int64_t));
+  assert(is_nonnull(data));
+
+  for (unsigned int i = 0; i < COUNT; i++) {
+    data[i] = (int64_t)rand() * (rand() % 2 ? 1 : -1);
+  }
+
+  int64_sort(data, COUNT);
+
+  {
+    int64_t previous = INT64_MIN;
+    int64_t current;
     for (unsigned int i = 0; i < COUNT; i++) {
       current = data[i];
       assert(current >= previous);
@@ -41,7 +90,8 @@ check_uint64_sort(void) {
   uint64_sort(data, COUNT);
 
   {
-    uint64_t previous = 0, current = 0;
+    uint64_t previous = 0;
+    uint64_t current;
     for (unsigned int i = 0; i < COUNT; i++) {
       current = data[i];
       assert(current >= previous);
@@ -54,7 +104,9 @@ check_uint64_sort(void) {
 
 int
 main(void) {
+  check_int32_sort();
   check_uint32_sort();
+  check_int64_sort();
   check_uint64_sort();
   return EXIT_SUCCESS;
 }
