c79eef28706432674a95e58192534f18718f28ad
Arto Bendiken <arto@bendiken.net>
2012-11-11 22:05:10 +0100

    Removed superfluous 'restrict' qualifiers from a header file.

--- src/str.h
+++ src/str.h
@@ -10,42 +10,42 @@ extern "C" {
 #include <stdbool.h> /* for bool */
 #include <stddef.h>  /* for size_t */
 
-extern char* str_dup(const char* restrict str);
+extern char* str_dup(const char* str);
 
-extern char* str_ndup(const char* restrict str,
+extern char* str_ndup(const char* str,
   size_t size);
 
-extern char* str_format(const char* restrict fmt, ...);
+extern char* str_format(const char* fmt, ...);
 
-extern long str_size(const char* restrict str);
+extern long str_size(const char* str);
 
-extern long str_length(const char* restrict str);
+extern long str_length(const char* str);
 
-extern hash_t str_hash(const char* restrict str);
+extern hash_t str_hash(const char* str);
 
 extern int str_compare(const char* str1, const char* str2);
 
 extern bool str_equal(const char* str1, const char* str2);
 
-extern bool str_is_empty(const char* restrict str);
+extern bool str_is_empty(const char* str);
 
-extern bool str_is_ascii(const char* restrict str);
-extern bool str_is_utf8(const char* restrict str);
+extern bool str_is_ascii(const char* str);
+extern bool str_is_utf8(const char* str);
 
-extern bool str_is_alnum(const char* restrict str);
-extern bool str_is_alpha(const char* restrict str);
-extern bool str_is_blank(const char* restrict str);
-extern bool str_is_cntrl(const char* restrict str);
-extern bool str_is_digit(const char* restrict str);
-extern bool str_is_graph(const char* restrict str);
-extern bool str_is_lower(const char* restrict str);
-extern bool str_is_print(const char* restrict str);
-extern bool str_is_punct(const char* restrict str);
-extern bool str_is_space(const char* restrict str);
-extern bool str_is_upper(const char* restrict str);
-extern bool str_is_xdigit(const char* restrict str);
+extern bool str_is_alnum(const char* str);
+extern bool str_is_alpha(const char* str);
+extern bool str_is_blank(const char* str);
+extern bool str_is_cntrl(const char* str);
+extern bool str_is_digit(const char* str);
+extern bool str_is_graph(const char* str);
+extern bool str_is_lower(const char* str);
+extern bool str_is_print(const char* str);
+extern bool str_is_punct(const char* str);
+extern bool str_is_space(const char* str);
+extern bool str_is_upper(const char* str);
+extern bool str_is_xdigit(const char* str);
 
-extern bool str_is_uuid(const char* restrict str);
+extern bool str_is_uuid(const char* str);
 
 extern bool str_has_prefix(const char* restrict str,
   const char* restrict prefix);
