adabe6d5b31ec673c131b88904eaa76acb1e07e0
Arto Bendiken <arto@bendiken.net>
2013-03-08 12:26:09 +0100

    Ensured that ./configure requires a C++11 compiler ($CXX -std=c++11).
    Imported the AX_CXX_COMPILE_STDCXX_11 macro from the Autoconf Archive.
    
    Source: http://www.gnu.org/software/autoconf-archive/ax_cxx_compile_stdcxx_11.html

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #/bin/sh
-#export CC='gcc -std=c1x'
-#export CXX='g++ -std=c++11'
-export CC='clang -std=c1x'
-export CXX='clang++ -std=c++11'
+#export CC='gcc'
+#export CXX='g++'
+export CC='clang'
+export CXX='clang++'
 export CPPFLAGS=''
 export CFLAGS='-g -Os'
 export CXXFLAGS='-g -Os'
