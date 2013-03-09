36ebcdc1ae0a898770f7c21eb683be852b9fde87
Arto Bendiken <arto@bendiken.net>
2013-03-09 01:28:34 +0100

    Implemented the cpr_vector_alloc() and cpr_vector_free() functions.

--- src/cpr/vector.cc
+++ src/cpr/vector.cc
@@ -6,10 +6,21 @@
 
 #include "cpr/vector.h"
 
-#include <vector> /* for std::vector */
+#include <cstdlib> /* for std::calloc(), std::free() */
+#include <vector>  /* for std::vector */
 
 struct cpr_vector : public std::vector<void*> {
   // TODO
 };
 
-extern const size_t cpr_vector_sizeof = sizeof(cpr_vector);
+extern const size_t cpr_vector_sizeof = sizeof(cpr_vector_t);
+
+cpr_vector_t*
+cpr_vector_alloc(void) {
+  return reinterpret_cast<cpr_vector_t*>(std::calloc(1, sizeof(cpr_vector_t)));
+}
+
+void
+cpr_vector_free(cpr_vector_t* const vector) {
+  std::free(vector);
+}
