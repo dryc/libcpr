046035092964f099e811c36fd40f5c9f7edf642f
Arto Bendiken <arto@bendiken.net>
2013-03-16 01:58:28 +0100

    Added a main page placeholder for the Doxygen documentation.

---
+++ doc/doxygen/mainpage.h
@@ -0,0 +1,8 @@
+/**
+ * @mainpage libcpr API Documentation
+ *
+ * This is API documentation automatically generated from the source code
+ * using the Doxygen documentation generation tool.
+ *
+ * @see http://libcpr.org/
+ */
