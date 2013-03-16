d4c207b9865d214ca3f95412bd4e9c7684508877
Arto Bendiken <arto@bendiken.net>
2013-03-17 00:01:17 +0100

    Added an example program that prints out the libcpr struct sizes.

---
+++ doc/examples/sizeof.c
@@ -0,0 +1,21 @@
+/**
+ * Example program that prints out the libcpr data structure sizes.
+ *
+ * Compile with: `cc -std=c11 -o sizeof sizeof.c -lcpr`
+ */
+
+#include <stdio.h>
+
+#include <cpr/list.h>
+#include <cpr/map.h>
+#include <cpr/set.h>
+#include <cpr/string.h>
+#include <cpr/vector.h>
+
+void main() {
+  printf("sizeof(cpr_list_t):\t%zu bytes\n",   cpr_list_sizeof);
+  printf("sizeof(cpr_map_t):\t%zu bytes\n",    cpr_map_sizeof);
+  printf("sizeof(cpr_set_t):\t%zu bytes\n",    cpr_set_sizeof);
+  printf("sizeof(cpr_string_t):\t%zu bytes\n", cpr_string_sizeof);
+  printf("sizeof(cpr_vector_t):\t%zu bytes\n", cpr_vector_sizeof);
+}
