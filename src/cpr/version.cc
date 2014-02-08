db63cbbc80fe3acb3038bb1365a4ad6baaaf8b54
Arto Bendiken <arto@bendiken.net>
2014-02-08 01:27:29 +0100

    Defined the cpr_version_{major,minor,patch} symbols.

--- src/cpr/version.cc
+++ src/cpr/version.cc
@@ -6,4 +6,10 @@
 
 #include "version.h"
 
-extern const char* const cpr_version_string = CPR_VERSION_STRING;
+const char* const cpr_version_string = CPR_VERSION_STRING;
+
+const short cpr_version_major = CPR_VERSION_MAJOR;
+
+const short cpr_version_minor = CPR_VERSION_MINOR;
+
+const short cpr_version_patch = CPR_VERSION_PATCH;
