07c0db5c32068fcfa3bc5d84e22d1dbee0ec9c24
Arto Bendiken <arto@bendiken.net>
2014-02-16 17:47:33 +0100

    Reordered header file function declarations in alphabetical order.

--- src/cpr/string.h
+++ src/cpr/string.h
@@ -46,13 +46,6 @@ extern const size_t cpr_string_npos;
 cpr_string_t* cpr_string(const char* str);
 
 /**
- * ...
- */
-cpr_string_t* cpr_string_substr(const char* str,
-  size_t position,
-  size_t length);
-
-/**
  * Allocates heap memory for a new `cpr_string_t` structure.
  *
  * @error  ENOMEM if the allocation of storage failed
@@ -64,12 +57,94 @@ cpr_string_t* cpr_string_substr(const char* str,
 cpr_string_t* cpr_string_alloc(void);
 
 /**
- * Deallocates the heap memory used by a string.
- *
- * @param  string a pointer to the string to deallocate, or a `NULL` pointer
- * @post   the `string` pointer is invalidated
+ * ...
  */
-void cpr_string_free(cpr_string_t* string);
+void cpr_string_append_char(cpr_string_t* string,
+  size_t count,
+  char character);
+
+/**
+ * ...
+ */
+void cpr_string_append_str(cpr_string_t* string,
+  const char* str,
+  size_t length);
+
+/**
+ * ...
+ */
+void cpr_string_append_string(cpr_string_t* string,
+  const cpr_string_t* suffix);
+
+/**
+ * ...
+ */
+void cpr_string_assign_char(cpr_string_t* string,
+  size_t count,
+  char character);
+
+/**
+ * ...
+ */
+void cpr_string_assign_str(cpr_string_t* string,
+  const char* str,
+  size_t length);
+
+/**
+ * ...
+ */
+char* cpr_string_at(cpr_string_t* string,
+  size_t position);
+
+/**
+ * Returns a pointer to the last character in a string.
+ *
+ * @param  string a pointer to the string to be accessed
+ * @pre    `string` is not a `NULL` pointer
+ * @error  EFAULT if `*string` is empty
+ * @return a pointer to the last character in `*string`
+ */
+char* cpr_string_back(cpr_string_t* string);
+
+/**
+ * Returns the size of allocated storage for a string.
+ *
+ * @param  string a pointer to the string to be accessed
+ * @pre    `string` is not a `NULL` pointer
+ * @return the current storage capacity of `*string`
+ */
+size_t cpr_string_capacity(const cpr_string_t* string);
+
+/**
+ * Removes all elements from a string, leaving its size zero.
+ *
+ * @param  string a pointer to the string to be mutated
+ * @pre    `string` is not a `NULL` pointer
+ * @post   the size of `*string` is zero
+ */
+void cpr_string_clear(cpr_string_t* string);
+
+/**
+ * ...
+ */
+int cpr_string_compare(const cpr_string_t* string, ...);
+
+/**
+ * ...
+ */
+size_t cpr_string_copy(const cpr_string_t* string, ...);
+
+/**
+ * Returns a direct pointer to a string's internal memory array.
+ *
+ * The returned pointer may be invalidated by further calls to other
+ * functions that mutate the string.
+ *
+ * @param  string a pointer to the string to be accessed
+ * @pre    `string` is not a `NULL` pointer
+ * @return a pointer to the C string representation of `*string`
+ */
+char* cpr_string_data(cpr_string_t* string);
 
 /**
  * Tests whether a string is empty, i.e., whether its size is zero.
@@ -82,13 +157,59 @@ void cpr_string_free(cpr_string_t* string);
 bool cpr_string_empty(const cpr_string_t* string);
 
 /**
- * Returns the byte length of a string.
+ * ...
+ */
+void cpr_string_erase(cpr_string_t* string,
+  size_t position,
+  size_t length);
+
+/**
+ * ...
+ */
+size_t cpr_string_find_char(const cpr_string_t* string,
+  char character,
+  size_t position);
+
+/**
+ * ...
+ */
+size_t cpr_string_find_str(const cpr_string_t* string,
+  const char* str,
+  size_t position);
+
+/**
+ * Deallocates the heap memory used by a string.
+ *
+ * @param  string a pointer to the string to deallocate, or a `NULL` pointer
+ * @post   the `string` pointer is invalidated
+ */
+void cpr_string_free(cpr_string_t* string);
+
+/**
+ * Returns a pointer to the first character in a string.
  *
  * @param  string a pointer to the string to be accessed
  * @pre    `string` is not a `NULL` pointer
- * @return the byte length of `*string`
+ * @error  EFAULT if `*string` is empty
+ * @return a pointer to the first character in `*string`
  */
-size_t cpr_string_size(const cpr_string_t* string);
+char* cpr_string_front(cpr_string_t* string);
+
+/**
+ * ...
+ */
+size_t cpr_string_insert_char(cpr_string_t* string,
+  size_t position,
+  char character,
+  size_t count);
+
+/**
+ * ...
+ */
+size_t cpr_string_insert_str(cpr_string_t* string,
+  size_t position,
+  const char* str,
+  size_t length);
 
 /**
  * Alias for `cpr_string_size()`.
@@ -109,22 +230,26 @@ size_t cpr_string_length(const cpr_string_t* string);
 size_t cpr_string_max_size(const cpr_string_t* string);
 
 /**
- * Returns the size of allocated storage for a string.
- *
- * @param  string a pointer to the string to be accessed
- * @pre    `string` is not a `NULL` pointer
- * @return the current storage capacity of `*string`
- */
-size_t cpr_string_capacity(const cpr_string_t* string);
-
-/**
- * Removes all elements from a string, leaving its size zero.
+ * Removes the last character of a string, reducing its size by one.
  *
  * @param  string a pointer to the string to be mutated
  * @pre    `string` is not a `NULL` pointer
- * @post   the size of `*string` is zero
+ * @error  EFAULT if `*string` is empty
+ * @post   if `*string` was not empty, its size is reduced by one
  */
-void cpr_string_clear(cpr_string_t* string);
+void cpr_string_pop_back(cpr_string_t* string);
+
+/**
+ * Appends a character to the end of a string, increasing its size by one.
+ *
+ * @param  string a pointer to the string to be mutated
+ * @param  character the character to append
+ * @pre    `string` is not a `NULL` pointer
+ * @error  EOVERFLOW if the length would exceed `max_size()`
+ * @error  ENOMEM if the allocation of storage failed
+ */
+void cpr_string_push_back(cpr_string_t* string,
+  char character);
 
 /**
  * Adjusts the minimum capacity of a string, allocating storage if necessary.
@@ -155,97 +280,6 @@ void cpr_string_resize(cpr_string_t* string,
   char character);
 
 /**
- * Removes the last character of a string, reducing its size by one.
- *
- * @param  string a pointer to the string to be mutated
- * @pre    `string` is not a `NULL` pointer
- * @error  EFAULT if `*string` is empty
- * @post   if `*string` was not empty, its size is reduced by one
- */
-void cpr_string_pop_back(cpr_string_t* string);
-
-/**
- * Appends a character to the end of a string, increasing its size by one.
- *
- * @param  string a pointer to the string to be mutated
- * @param  character the character to append
- * @pre    `string` is not a `NULL` pointer
- * @error  EOVERFLOW if the length would exceed `max_size()`
- * @error  ENOMEM if the allocation of storage failed
- */
-void cpr_string_push_back(cpr_string_t* string,
-  char character);
-
-/**
- * ...
- */
-char* cpr_string_at(cpr_string_t* string,
-  size_t position);
-
-/**
- * Returns a direct pointer to a string's internal memory array.
- *
- * The returned pointer may be invalidated by further calls to other
- * functions that mutate the string.
- *
- * @param  string a pointer to the string to be accessed
- * @pre    `string` is not a `NULL` pointer
- * @return a pointer to the C string representation of `*string`
- */
-char* cpr_string_data(cpr_string_t* string);
-
-/**
- * Returns a pointer to the first character in a string.
- *
- * @param  string a pointer to the string to be accessed
- * @pre    `string` is not a `NULL` pointer
- * @error  EFAULT if `*string` is empty
- * @return a pointer to the first character in `*string`
- */
-char* cpr_string_front(cpr_string_t* string);
-
-/**
- * Returns a pointer to the last character in a string.
- *
- * @param  string a pointer to the string to be accessed
- * @pre    `string` is not a `NULL` pointer
- * @error  EFAULT if `*string` is empty
- * @return a pointer to the last character in `*string`
- */
-char* cpr_string_back(cpr_string_t* string);
-
-/**
- * ...
- */
-int cpr_string_compare(const cpr_string_t* string);
-
-/**
- * ...
- */
-size_t cpr_string_copy(const cpr_string_t* string, ...);
-
-/**
- * ...
- */
-void cpr_string_erase(cpr_string_t* string,
-  size_t position,
-  size_t length);
-
-/**
- * ...
- */
-size_t cpr_string_find_char(const cpr_string_t* string,
-  char character,
-  size_t position);
-
-/**
- * ...
- */
-size_t cpr_string_find_str(const cpr_string_t* string,
-  const char* str,
-  size_t position);
-
-/**
  * ...
  */
 size_t cpr_string_rfind_char(const cpr_string_t* string,
@@ -260,47 +294,19 @@ size_t cpr_string_rfind_str(const cpr_string_t* string,
   size_t position);
 
 /**
- * ...
+ * Returns the byte length of a string.
+ *
+ * @param  string a pointer to the string to be accessed
+ * @pre    `string` is not a `NULL` pointer
+ * @return the byte length of `*string`
  */
-void cpr_string_append_char(cpr_string_t* string,
-  size_t count,
-  char character);
+size_t cpr_string_size(const cpr_string_t* string);
 
 /**
  * ...
  */
-void cpr_string_append_str(cpr_string_t* string,
-  const char* str,
-  size_t length);
-
-/**
- * ...
- */
-void cpr_string_assign_char(cpr_string_t* string,
-  size_t count,
-  char character);
-
-/**
- * ...
- */
-void cpr_string_assign_str(cpr_string_t* string,
-  const char* str,
-  size_t length);
-
-/**
- * ...
- */
-size_t cpr_string_insert_char(cpr_string_t* string,
+cpr_string_t* cpr_string_substr(const char* str,
   size_t position,
-  char character,
-  size_t count);
-
-/**
- * ...
- */
-size_t cpr_string_insert_str(cpr_string_t* string,
-  size_t position,
-  const char* str,
   size_t length);
 
 /**
