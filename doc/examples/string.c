a871c17b94469197aa18f3f64f45863a7db80792
Arto Bendiken <arto@bendiken.net>
2014-02-16 10:53:38 +0100

    Implemented an initial trivial dynamic string example program.

--- doc/examples/string.c
+++ doc/examples/string.c
@@ -4,5 +4,16 @@
 #include <cpr/string.h>
 
 int main(void) {
-  return EXIT_SUCCESS; // TODO
+  cpr_string_t* string = cpr_string("Numbers 0 to 9:\n");
+
+  for (int i = 0; i <= 9; i++) {
+    cpr_string_push_back(string, '0' + i);
+    cpr_string_push_back(string, '\n');
+  }
+
+  puts(cpr_string_data(string));
+
+  cpr_string_free(string);
+
+  return EXIT_SUCCESS;
 }
