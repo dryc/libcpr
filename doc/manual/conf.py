499b03a714946e6b1dcbf4297531c3e2bcad8817
Arto Bendiken <arto@bendiken.net>
2014-10-16 04:09:09 +0200

    Ensured that doc/manual/.themes/ is excluded from Sphinx.

--- doc/manual/conf.py
+++ doc/manual/conf.py
@@ -71,7 +71,7 @@ release = '2014-10-15'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['.build']
+exclude_patterns = ['.build', '.themes']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
